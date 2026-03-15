3f2e8d0ac00763c4948bc473939f41e167673a42
Joshua Adegbite <40638213+Horlladjosh@users.noreply.github.com>
2026-03-15 20:45:57 +0100

    Delete api directory

--- api/test.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function handler(req, res) {
-  res.status(200).json({ 
-    status: 'API is working!',
-    message: 'PDF generation endpoint is available',
-    timestamp: new Date().toISOString()
-  });
-}
